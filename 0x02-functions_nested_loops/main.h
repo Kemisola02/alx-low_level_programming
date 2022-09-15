83c518310330ed9d027949477ac740346e353e97
Kemisola02 <juliusmathar@gmail.com>
2022-09-14 21:34:39 -0700

    program that prints _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,19 @@
- 1 #ifndel MAIN_H
- 2 #define MAIN_H
- 3
- 4 int _putchar(char c);
- 5
- 6 #endif
+ #ifndef MAIN_H_INCLUDED
+ #define MAIN_H_INCLUDED
+
+ int _putchar(char c);
+ void print_alphabet(void);
+ void print_alphabet_x10(void);
+ int _islower(int c);
+ int _isalpha(int c);
+ int print_sign(int n);
+ int _abs(int n);
+ int print_last_digit(int n);
+ void jack_bauer(void);
+ void times_table(void);
+ int add(int a, int b);
+ void print_to_98(int n);
+ void print_times_table(int n);
+
+ #endif
 
