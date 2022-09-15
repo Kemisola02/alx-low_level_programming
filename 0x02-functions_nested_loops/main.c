6fb30b22c33a9ea281be09ebfed14906e76e539f
Kemisola02 <juliusmathar@gmail.com>
2022-09-14 20:50:35 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,14 @@
+ #include "main.h"
+
+/**
+* main - calls other funcs
+*
+* Return: always 0
+*/
+int main(void)
+{
+	printAlpha();
+
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+ 1 #ifndel MAIN_H
+ 2 #define MAIN_H
+ 3
+ 4 int _putchar(char c);
+ 5
+ 6 #endif
+
